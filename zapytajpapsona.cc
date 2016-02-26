cf5936925652f24ce2ab98ccbe0fa54e2d0d4ee2
putlennyhere <putlennyhere@users.noreply.github.com>
2016-02-27 00:51:18 +0100

    zapytajpapsona.cc

---
+++ zapytajpapsona.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+
+std::string papajpyta;
+
+using namespace std;
+int main() {
+	cout<<"Zadaj pytanie Papieżowi :3"<<endl;
+	cout<<"Drogi Papieżu: ";
+	cin>>papajpyta; 
+	srand (time(NULL));
+    std::string papsonmowi [6] = {"coo","jeszcze jak", "z warkoczykami albo bez warkoczyków", "ja nie wiem bo mi nie dali nigdy", "jak mi dadzą to zjem", "a po maturze chodziliśmy na kremówki"};
+    int kaszanka = rand() % 7;
+    //zultamorda
+    std::cout<<"████████████████████████"<<endl;
+    std::cout<<"████████"<<papsonmowi[kaszanka]<<"████████"<<endl;
+    std::cout<<"████████████████████████"<<endl;
+    std::cout<<"░░███░░░░░░░░░░░░░░░░"<<endl;
+	std::cout<<"░░░███░░░░░░░▄▄▀▀▀▀▀▀▄▄"<<endl;
+	std::cout<<"░░░░███░░░▄▄▀▄▄▄█████▄▄▀▄"<<endl;
+	std::cout<<"░░░░░██▄█▀▒▀▀▀█████████▄█▄"<<endl;
+	std::cout<<"░░░░░█▄██▒▒▒▒▒▒▒▒▀▒▀▒▀▄▒▀▒▀▄"<<endl;
+	std::cout<<"░░░░▄██▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▄"<<endl;
+	std::cout<<"░░░░██▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▌"<<endl;
+	std::cout<<"░░░▐██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐█"<<endl;
+	std::cout<<"░▄▄███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█"<<endl;
+	std::cout<<"▐▒▄▀██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐▌"<<endl;
+	std::cout<<"▌▒▒▌▒▀▒▒▒▒▒▒▄▀▀▄▄▒▒▒▒▒▒▒▒▒▒▒▒█▌"<<endl;
+	std::cout<<"▐▒▀▒▌▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒▀▀▀▀▄▒▐"<<endl;
+	std::cout<<"░█▒▒▌▒▒▒▒▒▒▒▒▀▀▒▀▒▒▐▒▄▀██▀▒▒▒▌"<<endl;
+	std::cout<<"░░█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▒▒▒▒█"<<endl;
+	std::cout<<"░░░▀▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌▒▒▒▒▒▒▄▀"<<endl;
+	std::cout<<"░░░▐▒▒▒▒▒▒▒▒▒▄▀▐▒▒▒▒▒▐▒▒▒▒▄▀"<<endl;
+	std::cout<<"░░▄▌▒▒▒▒▒▒▒▄▀▒▒▒▀▄▄▒▒▒▌▒▒▒▐▀▀▀▄▄▄"<<endl;
+	std::cout<<"▄▀░▀▄▒▒▒▒▒▒▒▒▀▀▄▄▄▒▄▄▀▌▒▒▒▌░░░░░░"<<endl;
+	std::cout<<"▐▌░░░▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▀░░░░░░░"<<endl;
+	std::cout<<"░█░░░░░▀▄▄▒▒▒▒▒▒▒▒▒▒▒▒▄▀░█░░░░░░░"<<endl;
+	std::cout<<"░░█░░░░░░░▀▄▄▄▒▒▒▒▒▒▄▀░░░░█░░░░░░"<<endl;
+
+}
